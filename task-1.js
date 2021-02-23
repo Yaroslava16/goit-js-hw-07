eb09e3f5d44a819be637d1b175a3673cd07048b8
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-02-23 21:19:34 +0200

    Решает первую задачу

---
+++ task-1.js
@@ -0,0 +1,8 @@
+const categoriesEl = document.querySelectorAll('.item');
+console.log(`В списке ${categoriesEl.length} категории.`);
+
+categoriesEl.forEach(item => {
+  console.log(`Категория: ${item.firstElementChild.textContent}`);
+
+  console.log(`Количество элементов: ${item.querySelectorAll('li').length}`);
+});
