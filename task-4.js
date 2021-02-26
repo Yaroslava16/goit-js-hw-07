d78a38aae87cabd9a8a26eb7b72668034f5d2af5
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-02-27 00:42:04 +0200

    Решает четвертую задачу

--- task-4.js
+++ task-4.js
@@ -1,34 +1,21 @@
 const counterValue = document.querySelector('#value');
 
+let counter = 0;
+
 const btnDecrement = document.getElementById('counter').firstElementChild;
 
 btnDecrement.addEventListener('click', onBtnDecrementClick);
 
 function onBtnDecrementClick() {
-  const value = btnDecrement.textContent;
-  console.log(value);
-  const magic = counterValue.textContent
-    .split('')
-    .reduce((total, num) => total + num, -1)
-    .splice(0, 1)
-    .join('');
-  console.log(magic);
-  return counterValue.append(...magic);
+  counter -= 1;
+  counterValue.textContent = counter;
 }
-
-console.log(btnDecrement);
 
 const btnIncrement = document.getElementById('counter').lastElementChild;
 
 btnIncrement.addEventListener('click', onBtnIncrementClick);
 
 function onBtnIncrementClick() {
-  const value = btnIncrement.textContent;
-  console.log(value);
+  counter += 1;
+  counterValue.textContent = counter;
 }
-
-console.log(btnIncrement);
-
-const increment = () => {};
-
-const decrement = () => {};
---
+++ task-5.js
@@ -0,0 +1,4 @@
+const input = document.querySelector('#name-input');
+
+const nameSpan = document.querySelector('#name-output');
+console.log(nameSpan);
