408ba37b815b80df51a150134bf22373e6fb1c6f
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-02-27 13:20:35 +0200

    Решает шестую задачку

--- task-6.js
+++ task-6.js
@@ -3,12 +3,13 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-  let inputLength = this.value.length;
-  let dataLength = this.getAttribute('data-length');
+  let inputLength = event.currentTarget.value.length;
+  let dataLength = Number(this.getAttribute('data-length'));
 
   if (inputLength === dataLength) {
-    input.classList.add('valid');
-  } else {
-    input.classList.add('invalid');
+    input.classList.remove('invalid');
+    return input.classList.add('valid');
   }
+  input.classList.remove('valid');
+  return input.classList.add('invalid');
 }
