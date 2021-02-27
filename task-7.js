ba07e957aedca7f2eed74a30d57a7f9b61e0f17e
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-02-27 14:07:48 +0200

    Решает седьмую задачу

---
+++ task-7.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#font-size-control');
+
+const span = document.querySelector('#text');
+
+input.addEventListener('input', onInputRange);
+
+function onInputRange(event) {
+  span.style.fontSize = event.currentTarget.value + 'px';
+}
